4d69346026a5632bd3f3950386afb76e3678d3df
Andrew Moore <andrew@lessthanseventy.com>
2023-08-26 17:51:03 -0600

    change dvd text

--- lua/zone/config.lua
+++ lua/zone/config.lua
@@ -1,48 +1,67 @@
 local default_opts = {
-    style = "treadmill",
-    after = 30,
-    offset = 3, -- The column offset for the fake float to open.
+	style = "treadmill",
+	after = 30,
+	offset = 3, -- The column offset for the fake float to open.
 
-    -- TODO: Config options for other styles and exclude filetypes
-    exclude_filetypes = {
-        'TelescopePrompt', 'NvimTree', 'dashboard', 'lir', 'neo-tree', 'help',
-    },
-    exclude_buftypes = {'terminal'},
+	-- TODO: Config options for other styles and exclude filetypes
+	exclude_filetypes = {
+		"TelescopePrompt",
+		"NvimTree",
+		"dashboard",
+		"lir",
+		"neo-tree",
+		"help",
+	},
+	exclude_buftypes = { "terminal" },
 
-    treadmill = {
-        direction = "left",  -- a lil buggy for `right`
-        tick_time = 30,      -- TODO: make it configurable inside helper.lua
-        headache = false
-    },
+	treadmill = {
+		direction = "left", -- a lil buggy for `right`
+		tick_time = 30, -- TODO: make it configurable inside helper.lua
+		headache = false,
+	},
 
-    matrix = {
-        tick_time = 50,
-        headache = false,
-    },
+	matrix = {
+		tick_time = 50,
+		headache = false,
+	},
 
-    epilepsy = {
-        stage = "aura",
-        tick_time = 100,
-        headache = false
-    },
+	epilepsy = {
+		stage = "aura",
+		tick_time = 100,
+		headache = false,
+	},
 
-    dvd = {
-        text = {
-        [[⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀]],
-        [[⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿]],
-        [[⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏]],
-        [[⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀]],
-        [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-        [[⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀]],
-        [[⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉⠀ ]],
-        [[                              ]],
-        }
-    },
+	dvd = {
+		text = {
+			[[    .,.                                                     ]],
+			[[  ..;l;..                                       ..          ]],
+			[[   .,c,.    .                          .',,;'       ..      ]],
+			[[    .'.           ..               .,,,,,..:;       ..      ]],
+			[[                  .             .,;;'.    .c'               ]],
+			[[           ..                .;;;'.....   ,:.               ]],
+			[[   .                       .;:;. ...  .. .c'  ..            ]],
+			[[  .;,.                    .c,     .......:;                 ]],
+			[[  .,'                .''';l,       ... .;:.                 ]],
+			[[                   .,,'..:;           .:;.                  ]],
+			[[.                 .;'...::.         .,:'                    ]],
+			[[...              .::''';l'       .',;,.                     ]],
+			[[...               ..   ,l;.  ..',;;,.            .          ]],
+			[[                      ';';c;,ldc. .'.       ',   ..         ]],
+			[[           ..,'.  .;:. .','..;d; .;'      .'cc,.            ]],
+			[[        .,cc:;ol.;l:. .,.   .:o:,;.       ..::..            ]],
+			[[      .:lc'   'll;. .:o;.   .,:,.           ..      .       ]],
+			[[     ;o:.         .;o:.                             .       ]],
+			[[    .'.          ,lc.                .                      ]],
+			[[               .cl'                  .     .                ]],
+			[[              .:;.                        ';.               ]],
+			[[                                          .'.               ]],
+		},
+	},
 
-    vanish = {
-        tick_time = 50,
-        headache = false,
-    }
+	vanish = {
+		tick_time = 50,
+		headache = false,
+	},
 }
 
 return default_opts
